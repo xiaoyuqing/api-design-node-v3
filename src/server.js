fc365761ca91f00893aecec324ce5f464326bcfc
xiaoyuqing <775706539@qq.com>
2019-02-18 14:47:10 +0800

    lesson-1

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
-
+const router = express.Router()
 app.disable('x-powered-by')
 
 app.use(cors())
@@ -12,4 +12,46 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+const log = (req, res, next) => {
+  console.log('logging')
+  req.myData = 'hello'
+  next()
+}
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+// cats
+const routes = [
+  'get /cat',
+  'get /cat:id',
+  'post /cat',
+  'put /cat/:id',
+  'delete /cat/:id'
+]
+router
+  .route('/cat')
+  .get()
+  .post()
+app.use('/api', router)
+
+app.get('/', log, (req, res, next) => {
+  // res.send({ data: 1 })
+  next()
+})
+
+app.get('/', log, (req, res) => {
+  res.send({ data: 2 })
+})
+
+app.put('/data', (req, res) => {})
+
+app.post('/data', (req, res) => {
+  console.log(req.body)
+  res.send({ ok: true })
+})
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
